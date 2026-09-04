0b82cc1ff37bb40e29e5b067547c9847341e1427
Social Boost horizon <exaucenapopolo2@gmail.com>
2026-09-04 11:31:21 +0100

    Update babel.config.js

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -1,13 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    presets: ['babel-preset-expo'],
     plugins: [
-      // Placé EN PREMIER pour supprimer la syntaxe TypeScript 'declare' avant les autres plugins
-      ["@babel/plugin-transform-typescript", { allowDeclareFields: true }],
-      ["@babel/plugin-transform-class-properties", { loose: true }],
-      ["@babel/plugin-transform-private-methods", { loose: true }],
-      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+      // Si vous utilisez react-native-reanimated, il doit TOUJOURS être le dernier plugin :
+      'react-native-reanimated/plugin',
     ],
   };
 };
