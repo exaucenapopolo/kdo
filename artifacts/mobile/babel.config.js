27ed0484517b4c8359f6ed90932c100c583988f8
Social Boost horizon <exaucenapopolo2@gmail.com>
2026-09-04 11:11:03 +0100

    Update babel.config.js

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -3,10 +3,8 @@ module.exports = function (api) {
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
     plugins: [
-      // Force-transform private class fields before hermesc sees them.
-      // The hermesc binary bundled with react-native on Linux (LLVM 8.0.0)
-      // does not support private class fields (ES2022), but react-native 0.81+
-      // ships DOMRect/DOMRectReadOnly with #x #y #width #height.
+      // Placé EN PREMIER pour supprimer la syntaxe TypeScript 'declare' avant les autres plugins
+      ["@babel/plugin-transform-typescript", { allowDeclareFields: true }],
       ["@babel/plugin-transform-class-properties", { loose: true }],
       ["@babel/plugin-transform-private-methods", { loose: true }],
       ["@babel/plugin-transform-private-property-in-object", { loose: true }],
